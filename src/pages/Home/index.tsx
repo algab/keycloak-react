ba9cbefad82450ba4a2aee335bf1def1c8b8f9d8
Alvaro Oliveira <algab.oliveira@gmail.com>
2021-06-20 13:21:48 -0300

    update page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useKeycloak } from '@react-keycloak/web';
+import { KeycloakProfile } from 'keycloak-js';
 
 import './styles.css';
 
 import logo from '../../logo.svg';
 
 const Home: React.FC = () => {
+  const [user, setUser] = useState<KeycloakProfile>();
+  const { keycloak } = useKeycloak();
+
+  useEffect(() => {
+    keycloak.loadUserProfile().then(profile => {
+      setUser(profile);
+    });
+  }, [keycloak]);
+
+  const logout = async () => {
+    await keycloak.logout();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Você está logado !
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {user?.firstName ? (
+          <p>
+            Olá {user?.firstName?.toUpperCase()}, você está logado !
+          </p>
+        ) : (
+          <p>
+            Você está logado !
+          </p>
+        )}
+        <button className="App-btn" onClick={logout}>
+          Sair
+        </button>
       </header>
     </div>
   );
