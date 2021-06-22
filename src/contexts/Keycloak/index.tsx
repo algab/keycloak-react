797d9499de53162fd5913d59fdc8496642475670
Alvaro Oliveira <alvaro.oliveira@compasso.com.br>
2021-06-22 14:21:05 -0300

    add env token refresh

--- src/contexts/Keycloak/index.tsx
+++ src/contexts/Keycloak/index.tsx
@@ -1,48 +1,60 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { KeycloakProfile } from 'keycloak-js';
+import React, { createContext, useEffect, useState } from "react";
+import { KeycloakProfile } from "keycloak-js";
 
-import Spinner from '../../components/Spinner';
-import { keycloak, keycloakProvider } from '../../utils/keycloak';
+import Spinner from "../../components/Spinner";
+import { keycloak, keycloakProvider } from "../../utils/keycloak";
 
 interface KeycloakCTX {
-  profile: KeycloakProfile | undefined
-  logout(): void 
+  profile: KeycloakProfile | undefined;
+  token: string;
+  logout(): void;
 }
 
 const KeycloakContext = createContext<KeycloakCTX>({} as KeycloakCTX);
 
 export const KeycloakProvider: React.FC = (props) => {
   const [profile, setProfile] = useState<KeycloakProfile>();
+  const [token, setToken] = useState<string>("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    function init() { 
+    function init() {
       setTimeout(async () => {
         await keycloak.init(keycloakProvider);
         if (keycloak.authenticated) {
           const profile = await keycloak.loadUserProfile();
           setProfile(profile as KeycloakProfile);
+          setToken(keycloak.token as string);
         }
         setLoading(false);
-      }, 1500);
+      }, 1000);
     }
     init();
   }, []);
-
-  keycloak.onAuthSuccess = async () => {
-    const profile = await keycloak.loadUserProfile();
-    setProfile(profile as KeycloakProfile);
-  };
 
   const logout = async () => {
     await keycloak.logout();
   };
 
+  keycloak.onAuthSuccess = async () => {
+    const profile = await keycloak.loadUserProfile();
+    setProfile(profile as KeycloakProfile);
+    setToken(keycloak.token as string);
+  };
+
+  keycloak.onTokenExpired = () => {
+    keycloak
+      .updateToken(Number(process.env.REACT_APP_TOKEN_REFRESH))
+      .then(() => {
+        setToken(keycloak.token as string);
+      });
+  };
+
   if (loading) {
-    return <Spinner />
+    return <Spinner />;
   }
   return (
-    <KeycloakContext.Provider value={{ profile, logout }}>
+    <KeycloakContext.Provider value={{ profile, token, logout }}>
       {props.children}
     </KeycloakContext.Provider>
   );
